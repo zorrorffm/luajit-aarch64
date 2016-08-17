b68f2988b1ab75f562b544d55be0dcf66c94693b
Zhongwei Yao <zhongwei.yao@linaro.org>
2016-08-17 17:59:04 +0800

    Add and change IR_TOSTR test cases.
    Reduce scope of tostr_char.lua to make it not generate IR_STRREF.
    
    Add tostr_int.lua.
    
    Change-Id: Icede2f76bfa4e9d1323653d14962ae556ffc068a

--- test/tostr_int.lua
+++ test/tostr_int.lua
@@ -1,8 +1,10 @@
--- test IR_TOSTR for char type.
+-- test IR_TOSTR for int type.
+x = 0
 
 for i = 1, 100 do
-   x = tostring(string.char(i))
+  x = tostring(i)
 end
 
-y = "d"
+y = "100"
+
 assert(x == y, "Got " .. x .. ", expect " .. y)
