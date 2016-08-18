zorrorffm/luajit-aarch64

commit f859ce80d620bb47fbc4105cfbfaad22594d5486

Fix the case while branch target is out of imm encoding range.
On some environment, printf is not resolved as printf@plt which is
out of imm encoding range.

Change-Id: I60a70bfe64aeb6bc9547762e4cee22fe2a9b9efc

src/lj_emit_arm64.h
@@ -302,12 +302,13 @@ static void emit_addptr(ASMState *as, Reg r, int32_t ofs)
 static void emit_call(ASMState *as, void *target)
 {
   static const int32_t imm_bits = 26;
-  MCode *p = --as->mcp;
-  ptrdiff_t delta = (char *)target - (char *)p;
-  delta >>= 2;
-  if ((delta + (1 << (imm_bits -1))) >= 0) {
-    *p = A64I_BL | (delta & ((1 << imm_bits) - 1));
-  } else {  /* Target out of range. */
-    lua_unimpl();
+  ptrdiff_t delta = (char *)target - (char *)(as->mcp - 1);
+  lua_assert(delta & 3 == 0);
+  if ((((delta >> 2) + (1 << (imm_bits - 1))) >> imm_bits) == 0) {
+    *--as->mcp = A64I_BL | ((delta >> 2) & ((1 << imm_bits) - 1));
+  } else {
+    /* Use LR for indirect calls. */
+    emit_n(as, A64I_BLR, RID_LR);
+    emit_loadu64(as, RID_LR, target);
   }
 }

src/lj_asm_arm64.h
@@ -391,6 +391,7 @@ static void asm_callx(ASMState *as, IRIns *ir)
   } else {
     /* Use LR for indirect calls. */
     emit_n(as, A64I_BLR, RID_LR);
+    emit_loadu64(as, RID_LR, func);
     ci.func = (ASMFunction)(void *)0;
   }
   asm_gencall(as, &ci, args);

commit d9d553514956ddcdb72e1533e0c7565764036c0d

Add parentheses to incorrect assert.
Fixes error introduced in f859ce80d620bb47fbc4105cfbfaad22594d5486.

src/lj_emit_arm64.h
@@ -308,7 +308,7 @@ static void emit_call(ASMState *as, void *target)
 {
   static const int32_t imm_bits = 26;
   ptrdiff_t delta = (char *)target - (char *)(as->mcp - 1);
-  lua_assert(delta & 3 == 0);
+  lua_assert((delta & 3) == 0);
   if ((((delta >> 2) + (1 << (imm_bits - 1))) >> imm_bits) == 0) {
     *--as->mcp = A64I_BL | ((delta >> 2) & ((1 << imm_bits) - 1));
   } else {