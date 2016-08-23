fcdd93fc7ee895c3381c580abce00bcdad63d8b1
Zhongwei Yao <zhongwei.yao@linaro.org>
2016-08-23 10:35:17 +0800

    Update xload test and disable cont_stitch on x86
    This update removes random failure when run xload_int.lua test. Now, it
    always fails on AArch64.
    
    cont_stitch.lua test fails on x86 and is disabled.
    
    Change-Id: I19e43e1357c97080392415d1327800106270a7c1

--- test/xload_int.lua
+++ test/xload_int.lua
@@ -1,8 +1,8 @@
 -- test IR_XLOAD for int type.
 ffi = require("ffi")
 
-ctype1 = ffi.typeof("int[10]")
-ctype2 = ffi.typeof("struct { int arr[10]; }")
+ctype1 = ffi.typeof("int[2]")
+ctype2 = ffi.typeof("struct { int arr[2]; }")
 z = 12
 
 -- initialize all x's element with z
@@ -14,4 +14,4 @@ for i = 1, 100 do
 end
 
 assert(y.arr[0] == z, "Got " .. y.arr[0] .. ", expect " .. z)
-assert(y.arr[9] == z, "Got " .. y.arr[9] .. ", expect " .. z)
+assert(y.arr[1] == z, "Got " .. y.arr[1] .. ", expect " .. z)
